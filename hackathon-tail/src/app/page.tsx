373717c3f899d4cb487adc2a60edab1e6fee6794
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2024-01-24 18:33:40 -0300

    Instalação de lib e limpeza de código

--- hackathon-tail/src/app/page.tsx
+++ hackathon-tail/src/app/page.tsx
@@ -1,8 +1,9 @@
 import FormLinkIdentifier from "./components/FormLinkIdentifier"
+import Puzzle from "./components/Puzzle";
 
 export default function Home() {
   return (
-    <FormLinkIdentifier />
+    <Puzzle/>
   );
 }
 
