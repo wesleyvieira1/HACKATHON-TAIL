c3eda1a6736091784cc908c1d63cd14d1bce44ed
pedroLucabral <pedro.simoes@academico.ufpb.br>
2024-01-24 21:56:28 -0300

    Download de músicas via youtube

--- hackathon-tail/src/app/api/music/route.tsx
+++ hackathon-tail/src/app/api/music/route.tsx
@@ -1,14 +1,28 @@
 import { NextResponse } from "next/server";
+const { Downloader } = require('ytdl-mp3');
+import ytdl from 'ytdl-core';
+import path from 'path';
+import * as fs from "fs";
 
 export async function POST(req : Request, res : string) {
-    const data =await req.json()
+    const data = await req.json()
 
-    if (data.linkidentificado.includes("spotify.com")) {
-        console.log("eh spotify")
-    } else if (data.linkidentificado.includes("youtube.com")) {
+    if (data.linkidentificado.includes("youtube.com")) {
         console.log("eh youtube")
-    } else if (data.linkidentificado.includes("deezer.com")) {
-        console.log("eh deezer")
+
+        let url = data.linkidentificado;
+        let videoId = ytdl.getURLVideoID(url);
+
+        const output = path.resolve('.\\public', "audio.mp3");
+        const video = ytdl(url, {filter:'audioonly'});
+        
+        video.pipe(fs.createWriteStream(output));
+
+        video.on("end", () => {
+            console.log("\nDownload Complete")
+
+        })
+
     }
 
     return NextResponse.json(data)
