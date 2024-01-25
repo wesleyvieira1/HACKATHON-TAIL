50f86f3a9c2067fb1ed6374a92a0edbb9808b69c
pedroLucabral <pedro.simoes@academico.ufpb.br>
2024-01-25 04:37:05 -0300

    configuração webpack

--- hackathon-tail/next.config.mjs
+++ hackathon-tail/next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import webpack from 'webpack';
+//const webpack = require('webpack')
+
+const nextConfig = {
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+      config.plugins.push(
+        new webpack.DefinePlugin({
+          'process.env.FLUENTFFMPEG_COV': false
+      })
+      )
+   
+      return config
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '*',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  }
 
 export default nextConfig;
