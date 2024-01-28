b06067fd02d02f8e0380ab52fde7de762364fa67
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2024-01-28 02:48:58 -0300

    add puzzle2

--- hackathon-tail/src/app/page.tsx
+++ hackathon-tail/src/app/page.tsx
@@ -1,10 +1,11 @@
 import DifficultScreen from "./components/DifficultScreen";
 import FormLinkIdentifier from "./components/FormLinkIdentifier"
 import Puzzle from "./components/Puzzle";
+import Puzzle2 from "./components/Puzzle2";
 
 export default function Home() {
   return (
-    <Puzzle/>
+    <Puzzle2/>
   );
 }
 
