765da97fecba2a521749ad880482b0b389227373
pedroLucabral <pedro.simoes@academico.ufpb.br>
2024-01-28 00:48:21 -0300

    receber faixas de audio divididas

--- hackathon-tail/src/app/api/music/route.tsx
+++ hackathon-tail/src/app/api/music/route.tsx
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 const { Downloader } = require('ytdl-mp3');
 import ytdl from 'ytdl-core';
-import Moises from "moises/sdk";
+import Moises from "moises/sdk.js";
 const ffmpeg = require('fluent-ffmpeg');
 const readline = require('readline');
 
@@ -17,21 +17,39 @@ export async function POST(req : Request, res : string) {
             quality: 'highestaudio',
           });
         
-        let directory = `./public/${videoId}.mp3`
+        let directory = `./public/input/${videoId}.mp3`;
           
         let start = Date.now();
-        ffmpeg(stream)
+        await ffmpeg(stream)
           .audioBitrate(128)
           .save(directory)
-          .on('progress', p => {
-            readline.cursorTo(process.stdout, 0);
-            process.stdout.write(`${p.targetSize}kb downloaded`);
+          .on("start", () => {
+            console.log("started download");
           })
           .on('end', () => {
             console.log(`\ndone - ${(Date.now() - start) / 1000}s`);
+
+            const key = `${process.env.API_KEY}`
+
+            const moises = new Moises({ apiKey : key});
+            moises.processFolder(
+              "TRACK_SEPARATION",
+              "./public/input",
+              "./public/output",
+              {}
+            )
+
         });
 
-    }
+          /*await moises.processFolder(
+            "GET_TIMESTAMPS",
+            "./public/input",
+            "./public/output",
+            {}
+        );*/
 
+        //const resultado = require (`C:/Users/Pedro/Documents/GitHub/HACKATHON-TAIL/hackathon-tail/public/output/${videoId}/RESULTADO.json`)
+        //console.log(resultado[0])
+    }
     return NextResponse.json(data)
 }
