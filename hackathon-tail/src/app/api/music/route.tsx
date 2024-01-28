ad10e35e9e7e5e4805a38b3185fbaf72ec17e2be
pedroLucabral <pedro.simoes@academico.ufpb.br>
2024-01-28 19:22:22 -0300

    PENÚLTIMO COMMIT SE DEUS QUISER

--- hackathon-tail/src/app/api/music/route.tsx
+++ hackathon-tail/src/app/api/music/route.tsx
@@ -39,7 +39,7 @@ async function getTracks(key : string) {
   moises.processFolder(
     "TRACK_SEPARATION",
     "./public/input",
-    "./public/output",
+    "./public/output/tracks",
     {}
   )
 }
@@ -64,17 +64,96 @@ async function getTimeStamp(key : string) {
   await moises.processFolder(
     "GET_TIMESTAMPS",
     `./public/output/tracks/${folderName[0]}`,
-    "./public/output/timestamps",
+    "./public/output/timestampsOne",
     {}
   );
   await moises.processFolder(
     "GET_TIMESTAMPS",
-    `./public/output/tracks/${folderName[1]},`,
-    "./public/output/timestamps",
+    `./public/output/tracks/${folderName[1]}`,
+    "./public/output/timestampsTwo",
     {}
   );
 
+  return folderName
     }
+
+async function cutAudios(file: string, start: number, output: string) {
+  ffmpeg()
+  .input(file)
+  .seekInput(start)
+  .duration(20)
+  .output(output)
+  .on("end", () => {
+    console.log("Cutting completed")
+  })
+  .run()
+}
+
+async function processAudios(folders : string[]) {
+  var count = 1
+  folders.splice(1, 1)
+  folders.forEach(element => {
+    if (count == 1) {
+      console.log(`count = ${count}`)
+      console.log(element)
+      const drumsData =  JSON.parse(fs.readFileSync('./public/output/timestampsOne/drums/RESULTADO.json'))
+      cutAudios(`./public/output/tracks/${element}/drums.wav`,parseInt(drumsData[0].start), `./public/output/tracks/cutTracks/${element}_drums.mp3`)
+      const guitarData =  JSON.parse(fs.readFileSync('./public/output/timestampsOne/guitars/RESULTADO.json'))
+      cutAudios(`./public/output/tracks/${element}/guitars.wav`,parseInt(guitarData[0].start), `./public/output/tracks/cutTracks/${element}_guitars.mp3`)
+      const pianoData =  JSON.parse(fs.readFileSync('./public/output/timestampsOne/piano/RESULTADO.json'))
+      cutAudios(`./public/output/tracks/${element}/piano.wav`,parseInt(pianoData[0].start), `./public/output/tracks/cutTracks/${element}_piano.mp3`)
+
+      const instrumentCutStamps = [drumsData, guitarData, pianoData]
+
+      instrumentCutStamps.forEach(element => {
+        console.log(element[0].start)
+      });
+
+      count++;
+    }
+    else if (count == 2) {
+      console.log(`count = ${count}`)
+      console.log(element)
+
+      const drumsData =  JSON.parse(fs.readFileSync('./public/output/timestampsTwo/drums/RESULTADO.json'))
+      cutAudios(`./public/output/tracks/${element}/drums.wav`,parseInt(drumsData[0].start), `./public/output/tracks/cutTracks/${element}_drums.mp3`)
+      const guitarData =  JSON.parse(fs.readFileSync('./public/output/timestampsTwo/guitars/RESULTADO.json'))
+      cutAudios(`./public/output/tracks/${element}/guitars.wav`,parseInt(guitarData[0].start), `./public/output/tracks/cutTracks/${element}_guitars.mp3`)
+      const pianoData =  JSON.parse(fs.readFileSync('./public/output/timestampsTwo/piano/RESULTADO.json'))
+      cutAudios(`./public/output/tracks/${element}/piano.wav`,parseInt(pianoData[0].start), `./public/output/tracks/cutTracks/${element}_piano.mp3`)
+
+      const instrumentCutStamps = [drumsData, guitarData, pianoData]
+
+      instrumentCutStamps.forEach(element => {
+        console.log(element[0].start)
+      })
+    }
+  });
+}
+
+async function uploadFiles(key : string) {
+  const files = fs.readdirSync("public/output/tracks/cutTracks")
+  const moises = new Moises({apiKey : key})
+  var allLinks: string[] = []
+
+  const downloadLink1 = await moises.uploadFile(`./public/output/tracks/cutTracks/${files[0]}`)
+  allLinks.push(downloadLink1)
+  const downloadLink2 = await moises.uploadFile(`./public/output/tracks/cutTracks/${files[1]}`)
+  allLinks.push(downloadLink1)
+  const downloadLink3 = await moises.uploadFile(`./public/output/tracks/cutTracks/${files[2]}`)
+  allLinks.push(downloadLink1)
+  const downloadLink4 = await moises.uploadFile(`./public/output/tracks/cutTracks/${files[3]}`)
+  allLinks.push(downloadLink1)
+  const downloadLink5 = await moises.uploadFile(`./public/output/tracks/cutTracks/${files[4]}`)
+  allLinks.push(downloadLink1)
+  const downloadLink6 = await moises.uploadFile(`./public/output/tracks/cutTracks/${files[5]}`)
+  allLinks.push(downloadLink1)
+  
+  return allLinks
+
+}
+
+
 
 async function getDelay(seconds : number) {
     return new Promise((resolve) => {
@@ -94,19 +173,22 @@ export async function POST(req : Request, res : string) {
 
     try {
 
-      //await downloadVideo(links[0]);
-      //await downloadVideo(links[1]);
-      //await getDelay(15);
+      await downloadVideo(links[0]);
+      await downloadVideo(links[1]);
+      await getDelay(20);
 
-      //await getTracks(`${process.env.API_KEY}`);
-      //await getDelay(25);
+      await getTracks(`${process.env.API_KEY}`);
+      await getDelay(30);
 
-      //await getTimeStamp(`${process.env.API_KEY}`);
-      //await getDelay(10)
+      const folders = await getTimeStamp(`${process.env.API_KEY}`);
+      await getDelay(20)
+      await processAudios(folders)
+      await getDelay(20)
+      const resData = await uploadFiles(`${process.env.API_KEY}`)
+      await getDelay(20)
 
-      return NextResponse.json({data})
+      return NextResponse.json({resData})
 
-      
     } catch (error) {
       console.log(error)
       return NextResponse.json({error})
