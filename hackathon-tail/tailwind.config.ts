ccac83160afae55874daaf851246dabbd7ee548e
wesleyvieira1 <wesley.henrique@academico.ifpb.edu.br>
2024-01-28 15:36:19 -0300

    style commit on welcome.tsx

--- hackathon-tail/tailwind.config.ts
+++ hackathon-tail/tailwind.config.ts
@@ -10,6 +10,15 @@ const config: Config = {
     extend: {
       colors:{
         'moisesai-color':'#00DAEB'
+      },
+      keyframes:{
+        slideDown:{
+          "0%":{transform : 'translateY(-50px)', opacity : "0" },
+          "100%":{transform : 'translateY(0px)', opacity : "1" }
+        }
+      },
+      animation: {
+        "slide-down" : "slideDown 0.5s ease-in-out 500ms",
       }
     },
   },
