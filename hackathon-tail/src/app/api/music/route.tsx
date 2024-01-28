978f826f9d3a0ebbe7ec54718cd868d9d7fa815d
pedroLucabral <pedro.simoes@academico.ufpb.br>
2024-01-28 12:49:07 -0300

    ORGANIZAÇÃO E TODO O RESTO

--- hackathon-tail/src/app/api/music/route.tsx
+++ hackathon-tail/src/app/api/music/route.tsx
@@ -2,52 +2,114 @@ import { NextResponse } from "next/server";
 import ytdl from 'ytdl-core';
 import Moises from "moises/sdk.js";
 const ffmpeg = require('fluent-ffmpeg');
+import fs from 'fs';
+
+async function downloadVideo(url : string) {
+    try {
+
+      let videoId = ytdl.getURLVideoID(url);
+
+      let stream = ytdl(videoId, {
+          quality: 'highestaudio',
+        });
+    
+      const directory = `./public/input/${videoId}.mp3`;
+        
+      let start = Date.now();
+
+      ffmpeg(stream)
+      .audioBitrate(128)
+      .save(directory)
+      .on("start", () => {
+      console.log("started download");
+      })
+      .on('end', () => {
+        console.log(`\ndone - ${(Date.now() - start) / 1000}s`);
+      });
+    }
+    catch(error) {
+      console.error(error)
+    }
+  };
+
+  
+
+async function getTracks(key : string) {
+  const moises = new Moises({ apiKey : key});
+  moises.processFolder(
+    "TRACK_SEPARATION",
+    "./public/input",
+    "./public/output",
+    {}
+  )
+}
+
+function getFolderNames(directory : string) {
+  fs.readdir(directory, (error, files) => {
+    if (error) {
+      console.error(error)
+    }
+    return files
+  })
+}
+
+async function getTimeStamp(key : string) {
+  const moises = new Moises({apiKey : key})
+ 
+  //const folderName: string[] = getFolderNames('public/output/tracks');
+  const folderName = fs.readdirSync('./public/output/tracks')
+  console.log(`./public/output/tracks/${folderName[0]}`)
+  console.log(`./public/output/tracks/${folderName[1]}`)
+
+  await moises.processFolder(
+    "GET_TIMESTAMPS",
+    `./public/output/tracks/${folderName[0]}`,
+    "./public/output/timestamps",
+    {}
+  );
+  await moises.processFolder(
+    "GET_TIMESTAMPS",
+    `./public/output/tracks/${folderName[1]},`,
+    "./public/output/timestamps",
+    {}
+  );
+
+    }
+
+async function getDelay(seconds : number) {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve('Async operation completed');
+      }, 1000 * seconds);
+    });
+  }
 
 export async function POST(req : Request, res : string) {
     const data = await req.json()
+    
+    let url = data.linkidentificado;
+    let links = [url, 'https://www.youtube.com/watch?v=8ESd3A9D-3E&ab_channel=BalaclavaRecords']
 
-    if (data.linkidentificado.includes("youtube.com") || data.linkidentificado.includes("youtu.be")) {
+    //console.log(links)
 
-        let url = data.linkidentificado;
-        let videoId = ytdl.getURLVideoID(url);
+    try {
 
-        let stream = ytdl(videoId, {
-            quality: 'highestaudio',
-          });
-        
-        let directory = `./public/input/${videoId}.mp3`;
-          
-        let start = Date.now();
-        await ffmpeg(stream)
-          .audioBitrate(128)
-          .save(directory)
-          .on("start", () => {
-            console.log("started download");
-          })
-          .on('end', () => {
-            console.log(`\ndone - ${(Date.now() - start) / 1000}s`);
+      //await downloadVideo(links[0]);
+      //await downloadVideo(links[1]);
+      //await getDelay(15);
 
-            const key = `${process.env.API_KEY}`
+      //await getTracks(`${process.env.API_KEY}`);
+      //await getDelay(25);
 
-            const moises = new Moises({ apiKey : key});
-            moises.processFolder(
-              "TRACK_SEPARATION",
-              "./public/input",
-              "./public/output",
-              {}
-            )
+      await getTimeStamp(`${process.env.API_KEY}`);
+      //await getDelay(10)
 
-        });
+      return NextResponse.json({data})
 
-          /*await moises.processFolder(
-            "GET_TIMESTAMPS",
-            "./public/input",
-            "./public/output",
-            {}
-        );*/
-
-        //const resultado = require (`C:/Users/Pedro/Documents/GitHub/HACKATHON-TAIL/hackathon-tail/public/output/${videoId}/RESULTADO.json`)
-        //console.log(resultado[0])
+      
+    } catch (error) {
+      console.log(error)
+      return NextResponse.json({error})
     }
-    return NextResponse.json(data)
-}
+
+    }
